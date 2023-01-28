d4a293f9e90ebc5391c0f5549c031a9fb6753120
Nobelgrant <104269270+Nobelgrant@users.noreply.github.com>
2023-01-28 11:49:53 +0630

    Add files via upload

---
+++ js/cardtypes.js
@@ -0,0 +1,59 @@
+export let cardtypes = [
+{
+    name: 'Minor Healing Potion',
+    value: 3,
+    image: 'healingpotion',
+    type: 'heal'
+},
+{
+    name: 'Major Healing Potion',
+    value: 6,
+    image: 'healingpotion',
+    type: 'heal'
+},
+{
+    name: 'Shield',
+    value: 5,
+    image: 'shield',
+    type: 'armor'
+},
+{
+    name: 'Kobold',
+    value: 6,
+    image: 'kobold',
+    type: 'attack'
+},
+{
+    name: 'Troll',
+    value: 8,
+    image: 'troll',
+    type: 'attack'
+},
+{
+    name: 'Ogre',
+    value: 10,
+    image: 'ogre',
+    type: 'attack'
+},
+{
+    name: 'Death Knight',
+    value: 12,
+    image: 'deathknight',
+    type: 'attack'
+},
+{
+    name: 'Dragon',
+    value: 16,
+    image: 'dragon',
+    type: 'attack'
+},
+{
+    name: 'Fire Drake',
+    value: 20,
+    image: 'firedrake',
+    type: 'attack'
+},
+
+];
+
+export default cardtypes;
